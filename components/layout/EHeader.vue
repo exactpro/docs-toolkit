af316dc58dab4ec0e452a29f29b69f2dad53aecc
Harsha Dharmabandu <harsha.dharmabandu@exactprosystems.com>
2023-03-22 09:14:20 +0000

    Update tailwindCSS theme

--- components/layout/EHeader.vue
+++ components/layout/EHeader.vue
@@ -1,22 +1,20 @@
 <template>
   <header class="w-full backdrop-blur">
     <div class="relative">
-      <div
-        class="bg-[url('~/assets/img/bg.png')] bg-cover absolute opacity-80 h-full w-full -z-10"
-      ></div>
+      <div class="e-header__background" />
       <div class="w-full flex justify-between items-center px-2 md:px-4">
         <div class="flex items-center">
           <button
-            class="w-7 h-w-7 text-tertiary-50 md:hidden"
+            class="w-7 h-w-7 text-white md:hidden"
             @click="$emit('update:showContentTree', !showContentTree)"
           >
             <IconsXMark v-if="showContentTree" class="w-full h-full" />
             <IconsBars3 v-else class="w-full h-full" />
           </button>
-          <NuxtLink to="/" class="flex items-center w-fit">
-            <img src="~/assets/img/logo.png" class="mb-2" />
+          <NuxtLink to="/" class="flex items-center w-fit gap-2">
+            <img src="~/assets/img/logo_white.svg" class="w-40 m-2" />
             <span
-              class="px-2 py-1 rounded-lg bg-primary-900 text-gray-50 font-semibold text-sm"
+              class="px-2 py-1 rounded-lg bg-accent-700 text-gray-50 font-semibold text-sm"
             >
               Docs
             </span>
@@ -42,3 +40,10 @@ export default defineComponent({
   }
 })
 </script>
+
+<style>
+.e-header__background {
+  @apply bg-primary opacity-95 absolute h-full w-full -z-10;
+  background: url('~/assets/img/bg.png') top left / cover;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,61 +1,53 @@
 const srcDir = '.'
 
+const defaultTheme = require('tailwindcss/defaultTheme') // eslint-disable-line @typescript-eslint/no-var-requires
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans]
+      },
       colors: {
         primary: {
-          DEFAULT: '#286455',
-          50: '#D1EDE6',
-          100: '#C3E7DE',
-          200: '#A6DBCE',
-          300: '#88D0BE',
-          400: '#6BC4AE',
-          500: '#4EB89E',
-          600: '#3F9E87',
-          700: '#34816E',
-          800: '#286455',
-          900: '#183C33'
+          DEFAULT: '#1E2736',
+          50: '#CCD5E3',
+          100: '#BFCADB',
+          200: '#A5B4CD',
+          300: '#8B9EBE',
+          400: '#7088B0',
+          500: '#58739F',
+          600: '#4A6085',
+          700: '#3B4D6A',
+          800: '#2D3A50',
+          900: '#1E2736'
         },
         secondary: {
-          DEFAULT: '#53B57F',
-          50: '#D3ECDF',
-          100: '#C5E6D4',
-          200: '#A9DABF',
-          300: '#8CCEA9',
-          400: '#70C194',
-          500: '#53B57F',
-          600: '#3F9164',
-          700: '#2E6A49',
-          800: '#1D432E',
-          900: '#0C1C13'
-        },
-        tertiary: {
-          DEFAULT: '#A3D3C5',
-          50: '#E8F4F1',
-          100: '#DAEDE8',
-          200: '#BFE0D6',
-          300: '#A3D3C5',
-          400: '#7DC1AD',
-          500: '#57AF95',
-          600: '#438B76',
-          700: '#306556',
-          800: '#1E3F36',
-          900: '#0C1915'
+          DEFAULT: '#5293C9',
+          50: '#DEEAF5',
+          100: '#CEE1F0',
+          200: '#AFCDE6',
+          300: '#90BADC',
+          400: '#71A6D3',
+          500: '#5293C9',
+          600: '#3677AD',
+          700: '#295A82',
+          800: '#1B3C57',
+          900: '#0E1F2D'
         },
         accent: {
-          DEFAULT: '#112A4D',
-          50: '#A5C2EB',
-          100: '#94B7E7',
-          200: '#73A0E0',
-          300: '#518AD9',
-          400: '#3073D1',
-          500: '#2761B1',
-          600: '#204E90',
-          700: '#183C6E',
-          800: '#112A4D',
-          900: '#07111F'
+          DEFAULT: '#3F979F',
+          50: '#D1EBED',
+          100: '#C3E4E7',
+          200: '#A6D7DC',
+          300: '#88CAD0',
+          400: '#6BBDC4',
+          500: '#4EB0B9',
+          600: '#3F979F',
+          700: '#2F7177',
+          800: '#1F4B4F',
+          900: '#0F2526'
         },
         error: {
           DEFAULT: '#B64C4C',
