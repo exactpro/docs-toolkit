93a5b63dab6a011ab8afc95239bec608326564fb
Stanislav <4eu3ep13@mail.ru>
2023-06-12 16:00:52 +0400

    Make linter happy

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -27,9 +27,7 @@ export default defineAppConfig({
         url: 'https://github.com/exactpro/docs-toolkit'
       }
     },
-    contacts: [
-
-    ],
+    contacts: [],
     seo: {
       robots: [{ UserAgent: '*' }, { Allow: '/' }],
       sitemap: {
