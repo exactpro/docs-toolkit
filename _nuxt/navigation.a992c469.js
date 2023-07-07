47b1b8d1c41ae56f9ab276148211d3da473efb37
StanG4313 <StanG4313@users.noreply.github.com>
2023-07-07 13:50:23 +0000

    deploy: c8223fe3692fdebd1d775b5a519f5235a12ef83f

---
+++ _nuxt/navigation.a992c469.js
@@ -0,0 +1,1 @@
+import{A as r}from"./entry.fc83830f.js";function t(i){return r(i)}export{t as w};
---
+++ features/features/term/_payload.js
@@ -0,0 +1,1 @@
+export default {data:{"page-data/features/features/term":null},prerenderedAt:void 0}
