66e4d04d54ce0df55d8c0b0b8b611ce4b066451f
Harsha Dharmabandu <harsha.dharmabandu@exactprosystems.com>
2023-07-18 20:42:40 +0530

    lintfix and add a required=true

--- components/ep/layout/Breadcrumbs.vue
+++ components/ep/layout/Breadcrumbs.vue
@@ -1,36 +1,52 @@
+<!--
+  ~ Copyright 2023 Exactpro (Exactpro Systems Limited)
+  ~
+  ~ Licensed under the Apache License, Version 2.0 (the "License");
+  ~ you may not use this file except in compliance with the License.
+  ~ You may obtain a copy of the License at
+  ~
+  ~     http://www.apache.org/licenses/LICENSE-2.0
+  ~
+  ~ Unless required by applicable law or agreed to in writing, software
+  ~ distributed under the License is distributed on an "AS IS" BASIS,
+  ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  ~ See the License for the specific language governing permissions and
+  ~ limitations under the License.
+  -->
+
 <template>
   <div>
-      <nav
-        v-if="breadcrumbs"
-        class="mb-4 flex flex-wrap items-center gap-x-2"
+    <nav v-if="breadcrumbs" class="mb-4 flex flex-wrap items-center gap-x-2">
+      <NuxtLink
+        class="border border-solid rounded text-gray-600 border-gray-600 hover:border-accent-600 hover:text-accent-600 transition-colors p-1 inline-flex"
+        to="/"
       >
-        <NuxtLink
-          class="border border-solid rounded text-gray-600 border-gray-600 hover:border-accent-600 hover:text-accent-600 transition-colors p-1 inline-flex"
-          to="/"
-        >
-          <Icon name="heroicons:home-20-solid" size="1.2em" />
-        </NuxtLink>
-        <Icon name="heroicons:chevron-right" />
+        <Icon name="heroicons:home-20-solid" size="1.2em" />
+      </NuxtLink>
+      <Icon name="heroicons:chevron-right" />
 
-        <template v-for="(crumb, index) in breadcrumbs" :key="crumb._path">
-          <template v-if="index < breadcrumbs.length - 1">
-            <NuxtLink class="underline text-accent-600" :to="crumb.path">
-              {{ crumb.title }}
-            </NuxtLink>
-            <Icon name="heroicons:chevron-right" />
-          </template>
-          <span v-else>
+      <template v-for="(crumb, index) in breadcrumbs" :key="crumb._path">
+        <template v-if="index < breadcrumbs.length - 1">
+          <NuxtLink class="underline text-accent-600" :to="crumb.path">
             {{ crumb.title }}
-          </span>
+          </NuxtLink>
+          <Icon name="heroicons:chevron-right" />
         </template>
-      </nav>
-    </div>
+        <span v-else>
+          {{ crumb.title }}
+        </span>
+      </template>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
-props: {
-  breadcrumbs: Array,
+  props: {
+    breadcrumbs: {
+      type: Array,
+      required: true
+    }
   }
 }
 </script>
