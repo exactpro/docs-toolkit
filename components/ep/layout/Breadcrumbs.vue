e56c904602fc34d0bce974df636284bdbcbbc30c
Harsha Dharmabandu <harsha.dharmabandu@exactprosystems.com>
2023-07-19 16:10:13 +0530

    remove v-if from vue comp, as v-if is in props

--- components/ep/layout/Breadcrumbs.vue
+++ components/ep/layout/Breadcrumbs.vue
@@ -15,7 +15,7 @@
   -->
 
 <template>
-  <nav v-if="breadcrumbs" class="mb-4 flex flex-wrap items-center gap-x-2">
+  <nav class="mb-4 flex flex-wrap items-center gap-x-2">
     <NuxtLink
       class="border border-solid rounded text-gray-600 border-gray-600 hover:border-accent-600 hover:text-accent-600 transition-colors p-1 inline-flex"
       to="/"
