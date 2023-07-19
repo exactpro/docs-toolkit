3e6272de2ee5d6529dee2ea56fb7adb24cf4b7df
Harsha Dharmabandu <harsha.dharmabandu@exactprosystems.com>
2023-07-19 15:28:39 +0530

    lintfix

--- components/ep/layout/Breadcrumbs.vue
+++ components/ep/layout/Breadcrumbs.vue
@@ -15,29 +15,27 @@
   -->
 
 <template>
-  
-    <nav v-if="breadcrumbs" class="mb-4 flex flex-wrap items-center gap-x-2">
-      <NuxtLink
-        class="border border-solid rounded text-gray-600 border-gray-600 hover:border-accent-600 hover:text-accent-600 transition-colors p-1 inline-flex"
-        to="/"
-      >
-        <Icon name="heroicons:home-20-solid" size="1.2em" />
-      </NuxtLink>
-      <Icon name="heroicons:chevron-right" />
+  <nav v-if="breadcrumbs" class="mb-4 flex flex-wrap items-center gap-x-2">
+    <NuxtLink
+      class="border border-solid rounded text-gray-600 border-gray-600 hover:border-accent-600 hover:text-accent-600 transition-colors p-1 inline-flex"
+      to="/"
+    >
+      <Icon name="heroicons:home-20-solid" size="1.2em" />
+    </NuxtLink>
+    <Icon name="heroicons:chevron-right" />
 
-      <template v-for="(crumb, index) in breadcrumbs" :key="crumb._path">
-        <template v-if="index < breadcrumbs.length - 1">
-          <NuxtLink class="underline text-accent-600" :to="crumb.path">
-            {{ crumb.title }}
-          </NuxtLink>
-          <Icon name="heroicons:chevron-right" />
-        </template>
-        <span v-else>
+    <template v-for="(crumb, index) in breadcrumbs" :key="crumb._path">
+      <template v-if="index < breadcrumbs.length - 1">
+        <NuxtLink class="underline text-accent-600" :to="crumb.path">
           {{ crumb.title }}
-        </span>
+        </NuxtLink>
+        <Icon name="heroicons:chevron-right" />
       </template>
-    </nav>
-  
+      <span v-else>
+        {{ crumb.title }}
+      </span>
+    </template>
+  </nav>
 </template>
 
 <script>
