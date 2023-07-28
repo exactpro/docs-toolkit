67c441daa2e80ba231e929bd56c324b1b9ddf6a4
Stanislav <4eu3ep13@mail.ru>
2023-07-28 13:39:00 +0400

    make linter happy

--- components/ep/layout/SurroundDocCard.vue
+++ components/ep/layout/SurroundDocCard.vue
@@ -33,7 +33,7 @@ defineProps<{
   <NuxtLink
     :to="withTrailingSlash(doc._path ?? '')"
     class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-    style="word-wrap: break-word; word-break: keep-all;"
+    style="word-wrap: break-word word-break: keep-all"
   >
     <div>
       <div v-if="direction === 'before'" class="mb-2">
